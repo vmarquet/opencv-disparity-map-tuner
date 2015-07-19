7a3d8c68989ca6ccb3879a46a21bc5d21864087e
vmarquet <vincent.marquet1@free.fr>
2015-07-19 13:19:45 +0900

    compute and display depth map, but no sliders in the GUI yet

--- StereoCorrespondenceBMTuner/mainwindow.cpp
+++ StereoCorrespondenceBMTuner/mainwindow.cpp
@@ -6,6 +6,14 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    bmState.state->preFilterSize=41;
+    bmState.state->preFilterCap=31;
+    bmState.state->SADWindowSize=41;
+    bmState.state->minDisparity=-64;
+    bmState.state->numberOfDisparities=128;
+    bmState.state->textureThreshold=10;
+    bmState.state->uniquenessRatio=15;
 }
 
 MainWindow::~MainWindow()
@@ -23,11 +31,21 @@ void MainWindow::on_pushButton_left_clicked()
     if (filename.isNull() || filename.isEmpty())
         return;
 
-    // we load the picture from the file
+    // we load the picture from the file, to display it in a QLabel in the GUI
     QImage left_picture;
     left_picture.load(filename);
 
     ui->label_image_left->setPixmap(QPixmap::fromImage(left_picture));
+
+    // we convert filename from QString to std::string (needed by OpenCV)
+    std::string filename_s = filename.toUtf8().constData();
+
+    // we load the picture in the OpenCV Mat format, to compute depth map
+    cv::Mat mat = cv::imread(filename_s, CV_LOAD_IMAGE_COLOR);
+    cv::cvtColor(mat, mat, CV_BGR2GRAY);  // we convert to gray, needed to compute depth map
+    this->left_image = mat;
+
+    compute_depth_map();
 }
 
 // method called when the button to change the right image is clicked
@@ -40,9 +58,59 @@ void MainWindow::on_pushButton_right_clicked()
     if (filename.isNull() || filename.isEmpty())
         return;
 
-    // we load the picture from the file
+    // we load the picture from the file, to display it in a QLabel in the GUI
     QImage right_picture;
     right_picture.load(filename);
 
     ui->label_image_right->setPixmap(QPixmap::fromImage(right_picture));
+
+    // we convert filename from QString to std::string (needed by OpenCV)
+    std::string filename_s = filename.toUtf8().constData();
+
+    // we load the picture in the OpenCV Mat format, to compute depth map
+    cv::Mat mat = cv::imread(filename_s, CV_LOAD_IMAGE_COLOR);
+    cv::cvtColor(mat, mat, CV_BGR2GRAY);  // we convert to gray, needed to compute depth map
+    this->right_image = mat;
+
+    compute_depth_map();
+}
+
+// we compute the depth map, if both left image and right image have been added
+void MainWindow::compute_depth_map() {
+    // we check that both images have been loaded
+    if (this->left_image.empty() || this->right_image.empty())
+        return;
+
+    // we check that both images have the same size (else OpenCV throws an error)
+    if (left_image.rows != right_image.rows || left_image.cols != right_image.cols) {
+        ui->label_depth_map->setText("Can't compute depth map: left and right images should be the same size");
+        return;
+    }
+
+    // we compute the depth map
+    cv::Mat disparity_16S;  // 16 bits, signed
+    bmState(left_image, right_image, disparity_16S);
+
+    // we convert the depth map to a QPixmap, to display it in the QUI
+    // first, we need to convert the disparity map to a more regular grayscale format
+    // then, we convert to RGB, and finally, we can convert to a QImage and then a QPixmap
+
+    // we normalize the values, so that they all fit in the range [0, 255]
+    cv::normalize(disparity_16S, disparity_16S, 0, 255, CV_MINMAX);
+
+    // we convert the values from 16 bits signed to 8 bits unsigned
+    cv::Mat disp(disparity_16S.rows, disparity_16S.cols, CV_8UC1);
+    for (int i=0; i<disparity_16S.rows; i++)
+        for (int j=0; j<disparity_16S.cols; j++)
+            disp.at<unsigned char>(i,j) = (unsigned char)disparity_16S.at<short>(i,j);
+
+    // we convert from gray to color
+    cv::Mat disp_color;
+    cv::cvtColor(disp, disp_color, CV_GRAY2RGB);
+
+    // we finally can convert the image to a QPixmap and display it
+    QImage disparity_image = QImage((unsigned char*) disp_color.data, disp_color.cols, disp_color.rows, QImage::Format_RGB888);
+    QPixmap disparity_pixmap = QPixmap::fromImage(disparity_image);
+
+    ui->label_depth_map->setPixmap(disparity_pixmap);
 }
--- StereoCorrespondenceBMTuner/mainwindow.h
+++ StereoCorrespondenceBMTuner/mainwindow.h
@@ -1,6 +1,12 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <iostream>
+
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
 #include <QMainWindow>
 #include <QFileDialog>
 
@@ -22,7 +28,17 @@ private slots:
     void on_pushButton_right_clicked();
 
 private:
+    // the UI object, to access the UI elements created with Qt Designer
     Ui::MainWindow *ui;
+
+    // the left and right pictures, converted to OpenCV Mat format
+    cv::Mat left_image;
+    cv::Mat right_image;
+
+    // the object that holds the parameters for the block-matching algorithm
+    cv::StereoBM bmState;
+
+    void compute_depth_map();
 };
 
 #endif // MAINWINDOW_H
